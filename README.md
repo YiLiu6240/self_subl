62940695bc628b1e7e33b5c0712409a5968b2984
yxl105 <yxl105@bham.ac.uk>
2015-07-17 13:47:10 +0100

    initial local commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # sublime_yxl105
+
+Personal package and theme/colorscheme for sublime text.
+
+## yxl105_package
+
+## Theme - Spacegray yxl105
+
+`Spacegray yxl105.sublime-theme`: modified from [Spacegray](https://github.com/kkga/spacegray) theme.
+
+`cobalt2 yxl105.tmTheme`: modified from [cobalt2](https://github.com/wesbos/cobalt2) theme. 
+
+Example:
+
+![example](/Theme - Spacegray yxl105/screenshots/example.png)
+
---
+++ yxl105_package/status_show_wrap.py
@@ -0,0 +1,31 @@
+# coding=utf8
+# referencing package: show files
+import sublime, sublime_plugin
+
+class OpenFilesPlugin(sublime_plugin.EventListener):
+
+    def __init__(self):
+        return
+
+    def on_activated(self, view):
+        self.update_status(view)
+    def on_new(self, view):
+        self.update_status(view)
+    def on_post_save(self, view):
+        self.update_status(view)
+    # def on_selection_modified(self, view):
+    #     self.update_status(view)
+
+    def update_status(self, view):
+        view_wrap_bool = view.settings().get('word_wrap')
+        if view_wrap_bool == True:
+            view_wrap = 'W'
+        else:
+            view_wrap = 'NW'
+        view_wrap_centered = view.settings().get('draw_centered')
+        view_wrap_width = view.settings().get('wrap_width')
+        if view_wrap_width == 0:
+            view_wrap_width = "Auto"
+        else:
+            view_wrap_width = str(view_wrap_width)
+        view.set_status('ShowWrapStatus', "[%s: %s%s]" % (view_wrap, view_wrap_width, ("C" if view_wrap_centered == True else "")) )
